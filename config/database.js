ee5443a8cbdc920ab53432ec05b2eb611b4d3043
Augusto Bennemann <augusto@augusto.dev>
2022-08-27 18:45:06 -0300

    Adjusts for deploying to Fly.io (5)

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", ""),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", false),
+      host: env("NF_DATABASE_HOST", "127.0.0.1"),
+      port: env.int("NF_DATABASE_PORT", 5432),
+      database: env("NF_DATABASE_DATABASE", ""),
+      user: env("NF_DATABASE_USERNAME", ""),
+      password: env("NF_DATABASE_PASSWORD", ""),
+      ssl: env.bool("NF_DATABASE_TLS_ENABLED", false),
     },
   },
 });
