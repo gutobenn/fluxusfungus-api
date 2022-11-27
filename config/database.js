ab4e9ba8a77e4b27db833ac8e7cb75fdcad5f4a1
Augusto Bennemann <augusto@augusto.dev>
2022-11-26 22:47:44 -0300

    Fixes

--- config/database.js
+++ config/database.js
@@ -1,13 +1,20 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("NF_DATABASE_HOST", "127.0.0.1"),
-      port: env.int("NF_DATABASE_PORT", 5432),
-      database: env("NF_DATABASE_DATABASE", ""),
-      user: env("NF_DATABASE_USERNAME", ""),
-      password: env("NF_DATABASE_PASSWORD", ""),
-      ssl: env.bool("NF_DATABASE_TLS_ENABLED", false),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: env('DATABASE_SSL', false)
+      },
+      options: {},
     },
   },
 });
+
